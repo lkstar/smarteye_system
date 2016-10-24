1f2a3ad73ea2de6c5399bc02f832501ba3191a8a
lishuliang <lishuliang@aliyun.com>
2016-10-24 18:53:00 +0800

    Add build information
    Signed-off-by: lishuliang <lishuliang@aliyun.com>

---
+++ OrangePi-Kernel/linux-3.4.112/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Mon Oct 24 17:37:58 CST 2016"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "shuliang"
+#define LINUX_COMPILER "gcc version 4.6.3 20120201 (prerelease) (crosstool-NG linaro-1.13.1-2012.02-20120222 - Linaro GCC 2012.02) "
